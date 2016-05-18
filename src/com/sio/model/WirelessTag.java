d732bf4a252fea70d836ca2d0b670fa88eae0e50
molihan <t0y@163.com>
2016-05-18 13:10:55 +0800

    fixed code

--- src/com/sio/model/WirelessTag.java
+++ src/com/sio/model/WirelessTag.java
@@ -9,13 +9,21 @@ package com.sio.model;
  *
  */
 public abstract class WirelessTag {
-
+	public static final String READY = "ready";
+	public static final String SENDING = "sending";
+	public static final String RECEIVED = "received";
+	public static final String COMPLETE = "complete";
+	public static final String RESEND = "resend";
+	public static final String FAIL = "fail";
 	/**
 	 *	Variable that contains Tag's MAC address.
 	 */
 	protected String mac;
 	protected byte[] broadcastInfo;
 	protected Tag tag;
+	protected boolean online;
+	protected String communication;
+	protected long update_serial; 
 	/**
 	 * This is an abstract function that must rewrite by other sub-classes.
 	 * And this function decides to use which method to send.
@@ -47,7 +55,23 @@ public abstract class WirelessTag {
 	}
 
 	public final void setTag(Tag tag) {
+		if(this.tag != null){
+			if(this.tag.code_1() != tag.code_1()){
+				communication = COMPLETE;
+			}
+		} else {
+			communication = READY;
+		}
+		online = true;
 		this.tag = tag;
+		
+	}
+	/**
+	 * READY ; SENDING ; RECEIVED ; COMPLETE ; RESEND ; FAIL ;
+	 * @return communication state.
+	 */
+	public final String getCommuncation(){
+		return communication;
 	}
 	
 	@Override
